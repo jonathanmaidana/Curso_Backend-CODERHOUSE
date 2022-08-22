13d5469dcf7be2f6d1f6b51f6fb087adb739a9be
Jonathan <jonathanmaidana24@gmail.com>
2022-08-22 11:01:39 -0300

    Desafio 7

--- Desafio7/public/index.js
+++ Desafio7/public/index.js
@@ -1,60 +1,82 @@
 const server = io().connect();
 
-// server.on('message', data => {
-//     console.log('message', data);
-// })
-
 server.on('message', (messages) => {
-    render(messages)
+    renderMsj(messages)
 });
 
-function render(messages){
+function renderMsj(messages){
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+
+/* A function that takes the messages from the server and renders them in the HTML. */
     console.log(messages)
     const html = messages.map(function (e,i) {
-        return(`<div>
-        <strong>${e.author}</strong>
-        <>${e.text}</>
+        return(`<div class="d-flex">
+        <strong class="me-1 color-primary mb-1">${e.email}</strong>
+        <div>
+        <span class="me-1 color-secondary mb-1">[${day}/${month}/${year} ${hour}:${minute}:${second}]:</span>
+        </div>
+        <span class="me-1 color-tertiary mb-1">${e.mensaje}</span>
         </div>`)
     }).join(" ");
 
     document.getElementById('messages').innerHTML = html;
 }
-server.on('message', function(data) {render(data)})
+server.on('message', function(data) {renderMsj(data)})
 
-function addMessage(e) {
+/**
+ * It takes the values of the email and message inputs, and sends them to the server.
+ * @param e - The event object
+ * @returns false.
+ */
+async function addMessage(e) {
     const mensaje = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
+        email: await document.getElementById('email').value,
+        mensaje: await document.getElementById('mensaje').value
     };
+    console.log(mensaje)
     server.emit('new-message', mensaje);
     return false;
 }
 
-server.on('container', (container) => {
-    render(container)
+/* Listening for a new product to be added to the database. */
+server.on('product', (container) => {
+    renderPdct(container)
 })
 
-function render(container){
+/* Rendering the products in the table. */
+function renderPdct(container){
     console.log(container)
     const html = container.map(function(e,i) {
         return(`<tr>
                 <td class="pb-2" id="title">${e.title}</td>
-                <td class="pb-2" id="price">${e.price}</td>
+                <td class="pb-2" id="price">$${e.price}</td>
                 <td class="pb-2 d-flex justify-content-center">
-                    <img style="width: 30px" id="thumbnail" src=${e.thumbnail}/>
+                    <img style="width: 30px" id="thumbnail" src="${e.thumbnail}"/>
                 </td>
                 </tr>`)
     }).join(" ");
 
     document.getElementById('products').innerHTML = html;
 }
-server.on('container', function(data) {render(data)})
+server.on('product', function(data) {renderPdct(data)})
 
-function addProduct(e) {
+/**
+ * It takes the values from the form and sends them to the server.
+ * @param e - the event object
+ * @returns false.
+ */
+async function addProduct(e) {
     const product = {
-        title: document.getElementById('title-form').value,
-        price: document.getElementById('price-form').value,
-        thumbnail: document.getElementById('thumbnail-form').value
+        title: await document.getElementById('title-form').value,
+        price: await document.getElementById('price-form').value,
+        thumbnail: await document.getElementById('thumbnail-form').value
     };
     server.emit('new-product', product);
     return false;
--- Desafio7/server.js
+++ Desafio7/server.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
 let messages = [
-    { author: "Juan", text: "¡Hola! ¿Que tal?" },
-    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
-    { author: "Ana", text: "¡Genial!" }
+    { email: "Juan", mensaje: "¡Hola! ¿Que tal?" },
+    { email: "Pedro", mensaje: "¡Muy bien! ¿Y vos?" },
+    { email: "Ana", mensaje: "¡Genial!" }
 ]; 
 
 app.get('/', async (req, res) => {
@@ -25,24 +25,27 @@ app.get('/', async (req, res) => {
     })
 
 io.on('connection', async (socket) => {
-
     /* Logging to the console that the client has connected. */
-    console.log('El cliente se ha conectado');
+    console.log('El usuario se ha conectado');
+    socket.on('disconnect', () => {
+        console.log('El usuario se ha desconectado')  
+    })
     const getAll = await contenedor.getAll()
     /* Sending the messages array to the client. */
     socket.emit('message', messages);
-    socket.emit('container', getAll);
+    socket.emit('product', getAll);
     /* Listening for a new message from the client and then pushing it to the messages array and then
     emitting the new message to all the clients. */
-    socket.on('new-message', (msj) => {
-        messages.push(msj);
+    socket.on('new-message', (data) => {
+        messages.push(data);
         io.sockets.emit('message', messages);
-    }) 
-    
+    })
+    /* Listening for a new product from the client and then pushing it to the product array and then
+        emitting the new product to all the clients. */
     socket.on('new-product', (data) =>{
-        contenedor.newObj(data)
-        io.sockets.emit('container', getAll)
-    } )
+       contenedor.newObj(data)
+        io.sockets.emit('product', getAll)
+    }) 
 })
 
 const PORT = 3000;
